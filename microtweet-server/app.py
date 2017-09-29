ce3a28bfb115e0f500336c750e27b27b859bac69
dario <darionovantadue@gmail.com>
2017-09-29 10:48:10 +0200

    Todo microservice

--- microtweet-server/app.py
+++ microtweet-server/app.py
@@ -1,34 +1,34 @@
-import os
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from logging import getLogger
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['SECRET']
+app.config['SECRET_KEY'] = 'secret!'
+log = getLogger(__name__)
+
 socketio = SocketIO(app)
 
-
 @app.route('/')
-def home():
+def index():
+    log.info("Request for index")
     return render_template('index.html')
 
 @socketio.on('new_tweet', namespace='/tweet')
-def new_tweet(tweet):
-    print(tweet)
-    emit('ok')
+def test_message(message):
+    log.info("New tweet received")
+    emit('tweet', {'data': message['data']}, broadcast=True)
 
 
 @socketio.on('connect', namespace='/tweet')
-def connect():
-    print("Connected")
-    emit('client_connected', broadcast=True)
+def test_connect():
+    log.info("New user connected")
+    emit('log_message', {'data': 'Connected'})
 
 @socketio.on('disconnect', namespace='/tweet')
-def disconnect():
-    print("Disconnected")
-    emit('client_disconnected', broadcast=True)
-
+def test_disconnect():
+    log.info("User disconnected")
+    emit("log_message", {'data': 'Disconnected'})
 
 if __name__ == '__main__':
     socketio.run(app)
-
-
